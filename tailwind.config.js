0e17fe8d2156d961f5e0a0531862863739805aee
danielhamilton <307828+danielhamilton@users.noreply.github.com>
2021-12-07 23:41:55 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.html',
+    './src/**/*.js',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        rose: colors.rose,
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-children'),
+
+  ],
+}
